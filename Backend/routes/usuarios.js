8befd918e5cc7287bceffd0c8228f06548c56463
ElPanaBru <brunomorenomarkman@gmail.com>
2026-08-12 16:40:36 -0300

    No tocar. Ultimo cambio

--- Backend/routes/usuarios.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usuariosController = require('../Controllers/usuarioController');
-
-router.get('/verificar/:uid', usuariosController.verificarUsuario);
-router.post('/registrar', usuariosController.registrarUsuario);
-router.post('/convertir-a-musico', usuariosController.convertirAMusico);
-
-module.exports = router;
