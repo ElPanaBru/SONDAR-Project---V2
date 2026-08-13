9f53456762a7986002de4a7440d69df28c1947e5
unknown <jfrancope808@gmail.com>
2026-08-13 09:46:03 -0300

    67
    aaaaa logo

--- Frontend/src/componentes/Navbar.jsx
+++ Frontend/src/componentes/Navbar.jsx
@@ -284,7 +284,7 @@ function Navbar({ usuario }) {
     <>
       <nav className="custom-navbar">
         <Link to="/" className="navbar-brand">
-          <img src="/logo/sondar-logo.png" alt="SONDAR" className="logo" />
+          <img src="/logo/sondar-logo.png" alt="SONDAR" className="sondar-brand-image logo" />
         </Link>
 
         <div className="navbar-center">
